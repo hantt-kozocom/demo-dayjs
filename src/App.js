cf5cc3de675637cecd849b99e9be1718ef7d9bfd
hantt-kozocom <hantt@kozo-japan.com>
2023-03-10 15:56:22 +0700

    'dayjs'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import dayjs from "dayjs";
+import range from "lodash-es/range";
+import "./style.css";
 
-function App() {
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const todayObj = dayjs();
+
+const App = () => {
+  const [dayObj, setDayObj] = useState(dayjs());
+
+  const thisYear = dayObj.year();
+  const thisMonth = dayObj.month(); // (January as 0, December as 11)
+  const daysInMonth = dayObj.daysInMonth();
+
+  const dayObjOf1 = dayjs(`${thisYear}-${thisMonth + 1}-1`);
+  const weekDayOf1 = dayObjOf1.day(); // (Sunday as 0, Saturday as 6)
+
+  const dayObjOfLast = dayjs(`${thisYear}-${thisMonth + 1}-${daysInMonth}`);
+  const weekDayOfLast = dayObjOfLast.day();
+
+  const handlePrev = () => {
+    setDayObj(dayObj.subtract(1, "month"));
+  };
+
+  const handleNext = () => {
+    setDayObj(dayObj.add(1, "month"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calendar">
+      <div className="header">
+        <button type="button" className="nav nav--prev" onClick={handlePrev}>
+          {"<"}
+        </button>
+        <div className="datetime">{dayObj.format("MMM DD YYYY")}</div>
+        <button type="button" className="nav nav--prev" onClick={handleNext}>
+          >
+        </button>
+      </div>
+      <div className="week-container">
+        {weekDays.map((d) => (
+          <div className="week-cell" key={d}>
+            {d}
+          </div>
+        ))}
+      </div>
+      <div className="day-container">
+        {range(weekDayOf1).map((i) => (
+          <div className="day-cell day-cell--faded" key={i}>
+            {dayObjOf1.subtract(weekDayOf1 - i, "day").date()}
+          </div>
+        ))}
+
+        {range(daysInMonth).map((i) => (
+          <div
+            className={`day-cell day-cell--in-month${
+              i + 1 === todayObj.date() &&
+              thisMonth === todayObj.month() &&
+              thisYear === todayObj.year()
+                ? " day-cell--today"
+                : ""
+            }`}
+            key={i}
+          >
+            {i + 1}
+          </div>
+        ))}
+
+        {range(6 - weekDayOfLast).map((i) => (
+          <div className="day-cell day-cell--faded" key={i}>
+            {dayObjOfLast.add(i + 1, "day").date()}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
